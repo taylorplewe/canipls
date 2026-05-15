ea3b8364fcf21f5418c68b5162d7a4d9dc47478c
Taylor Plewe <tplewe@outlook.com>
2026-05-14 21:45:07 -0600

    use abstracted hover function for javascript

--- src/parsers/js.zig
+++ src/parsers/js.zig
@@ -78,107 +78,35 @@ fn getHoverInfoAtPosition(
     row: u32,
 ) ?HoverInfo {
     const QUERY_IDENTIFIERS = "(identifier) @name";
-    const QUERY_JSX_TAGS =
-        \\[
-        \\  (jsx_opening_element
-        \\    (identifier) @tagname
-        \\  )
-        \\  (jsx_closing_element
-        \\    (identifier) @tagname
-        \\  )
-        \\]
-    ;
-    const QUERY_JSX_ATTRS =
-        \\(jsx_attribute
-        \\  (property_identifier) @attrname
-        \\)
-    ;
+    const QUERY_JSX_TAGS = "(jsx_opening_element (identifier) @tagname)";
+    const QUERY_JSX_ATTRS = "(jsx_attribute (property_identifier) @attrname)";
 
-    const parser = ts.Parser.create();
-    defer parser.destroy();
-    parser.setLanguage(lang_javascript) catch return null;
+    const symbols = [_]types.SymbolInfo{
+        .{
+            .element_kind = .JsApi,
+            .support_bin = js_identifiers_bin,
+            .ts_query_text = QUERY_IDENTIFIERS,
+        },
+        .{
+            .element_kind = .HtmlElement,
+            .support_bin = html.html_tags_bin,
+            .ts_query_text = QUERY_JSX_TAGS,
+        },
+        .{
+            .element_kind = .HtmlAttribute,
+            .support_bin = html.html_attributes_bin,
+            .ts_query_text = QUERY_JSX_ATTRS,
+        },
+    };
 
-    const parse_res = parser.parseString(code, null);
-    if (parse_res) |ast| {
-        defer ast.destroy();
-
-        const root_node = ast.rootNode();
-
-        var error_offset: u32 = 0;
-        const query_identifiers = ts.Query.create(lang_javascript, QUERY_IDENTIFIERS, &error_offset) catch |err| {
-            log.err("could not create tree-sitter query: {}", .{err});
-            return null;
-        };
-        defer query_identifiers.destroy();
-        const query_jsx_tags = ts.Query.create(lang_javascript, QUERY_JSX_TAGS, &error_offset) catch |err| {
-            log.err("could not create tree-sitter query: {}", .{err});
-            return null;
-        };
-        defer query_jsx_tags.destroy();
-        const query_jsx_attrs = ts.Query.create(lang_javascript, QUERY_JSX_ATTRS, &error_offset) catch |err| {
-            log.err("could not create tree-sitter query: {}", .{err});
-            return null;
-        };
-        defer query_jsx_attrs.destroy();
-
-        const cursor = ts.QueryCursor.create();
-        defer cursor.destroy();
-
-        cursor.setPointRange(
-            .{ .column = column, .row = row },
-            .{ .column = column, .row = row },
-        ) catch return null;
-
-        // identifiers
-        cursor.exec(query_identifiers, root_node);
-        while (cursor.nextMatch()) |match| {
-            const identifier_node = match.captures[0].node;
-            const identifier_name = code[identifier_node.startByte()..identifier_node.endByte()];
-
-            const maybe_attr_support_percentage = Parser.getSupportPercentageForIdentifierFromBin(identifier_name, js_identifiers_bin);
-            if (maybe_attr_support_percentage) |percentage| {
-                return HoverInfo{
-                    .caniuse_id = "html_elements_geolocation", // TEMP
-                    .identifier = identifier_name,
-                    .support_percentage = percentage,
-                };
-            }
-        }
-
-        // JSX attributes
-        cursor.exec(query_jsx_tags, root_node);
-        while (cursor.nextMatch()) |match| {
-            const attr_node = match.captures[0].node;
-            const attr_name = code[attr_node.startByte()..attr_node.endByte()];
-
-            const maybe_attr_support_percentage = Parser.getSupportPercentageForIdentifierFromBin(attr_name, html_attributes_bin);
-            if (maybe_attr_support_percentage) |percentage| {
-                return HoverInfo{
-                    .caniuse_id = "html_elements_geolocation", // TEMP
-                    .identifier = attr_name,
-                    .support_percentage = percentage,
-                };
-            }
-        }
-
-        // JSX elements
-        cursor.exec(query_jsx_tags, root_node);
-        while (cursor.nextMatch()) |match| {
-            const tag_node = match.captures[0].node;
-            const tag_name = code[tag_node.startByte()..tag_node.endByte()];
-
-            const maybe_tag_support_percentage = Parser.getSupportPercentageForIdentifierFromBin(tag_name, html_tags_bin);
-            if (maybe_tag_support_percentage) |percentage| {
-                return HoverInfo{
-                    .caniuse_id = "html_elements_geolocation", // TEMP
-                    .identifier = tag_name,
-                    .support_percentage = percentage,
-                };
-            }
-        }
-    }
-
-    return null;
+    return Parser.getHoverDocFromCodeAtPosition(
+        lang_javascript,
+        code,
+        column,
+        row,
+        &symbols,
+        &.{},
+    );
 }
 
 fn trimComment(comment_raw: []const u8) []const u8 {
