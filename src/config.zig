b54bb530370bd4ef0a185a04f0ce64a7bd6842f0
Taylor Plewe <tplewe@outlook.com>
2026-06-13 13:20:44 -0600

    change name to ignored_feature_ids

--- src/config.zig
+++ src/config.zig
@@ -12,19 +12,20 @@ const builtin = @import("builtin");
 
 const log = std.log.scoped(.canipls);
 
+const CONFIG_FILE_NAME = "canipls.json";
+
 /// The fields in the user's config file should be formatted the exact same way as the fields in this struct:
 const Config = struct {
     support_threshold: ?f32 = null,
     show_low_support_warnings: ?bool = null,
-    ignored_features: ?[][]const u8 = null,
+    ignored_feature_ids: ?[][]const u8 = null,
 };
 pub var config: Config = .{
     .support_threshold = 90.0,
     .show_low_support_warnings = true,
-    .ignored_features = &.{},
+    .ignored_feature_ids = &.{},
 };
 
-const CONFIG_FILE_NAME = "canipls.json";
 const SetConfigError = error{
     NoAppDataEnv,
     NoHomeEnv,
@@ -76,7 +77,9 @@ pub fn applyConfigFileToGlobalConfig(file_bytes: []const u8) void {
         Config,
         arena.allocator(),
         file_bytes,
-        .{ .ignore_unknown_fields = true, .duplicate_field_behavior = .use_last },
+        .{
+            .duplicate_field_behavior = .use_last,
+        },
     ) catch |err| {
         log.err("could not apply config file to global config: {}", .{err});
         return;
@@ -89,46 +92,4 @@ pub fn applyConfigFileToGlobalConfig(file_bytes: []const u8) void {
             @field(config, field.name) = val;
         }
     }
-
-    // var lines_it = std.mem.tokenizeAny(u8, file_bytes, "\r\n");
-    // var line_index: usize = 1;
-    // line_loop: while (lines_it.next()) |line| : (line_index += 1) {
-    //     var key_value_it = std.mem.tokenizeAny(u8, line, " \t");
-    //     const key = key_value_it.next() orelse {
-    //         log.warn("no key found on line {d} of config file", .{line_index});
-    //         continue :line_loop;
-    //     };
-    //     const value = key_value_it.next() orelse {
-    //         log.warn("no value found on line {d} of config file", .{line_index});
-    //         continue :line_loop;
-    //     };
-
-    //     inline for (@typeInfo(Config).@"struct".fields) |field| {
-    //         if (std.mem.eql(u8, field.name, key)) {
-    //             switch (field.type) {
-    //                 f32 => {
-    //                     const f32_val = std.fmt.parseFloat(f32, value) catch |err| {
-    //                         log.warn("value on line {d} is not a valid float; ignoring line. Error: {}", .{ line_index, err });
-    //                         continue :line_loop;
-    //                     };
-    //                     @field(config, field.name) = f32_val;
-    //                 },
-    //                 bool => {
-    //                     const bool_val = if (std.mem.eql(u8, value, "true"))
-    //                         true
-    //                     else if (std.mem.eql(u8, value, "false"))
-    //                         false
-    //                     else {
-    //                         log.warn("value on line {d} is not 'true' or 'false'; ignoring line.", .{line_index});
-    //                         continue :line_loop;
-    //                     };
-    //                     @field(config, field.name) = bool_val;
-    //                 },
-    //                 else => {},
-    //             }
-    //             continue :line_loop;
-    //         }
-    //     }
-    //     log.warn("no config field called '{s}' -- ignoring", .{key});
-    // }
 }
